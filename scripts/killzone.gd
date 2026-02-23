2b2ee35ffc9c798218256058f7bb9e39862e14b0
sergingameprays <sergio.sivonei@hotmail.com>
2026-02-23 16:40:48 -0300

    feat: map, enemy and background

---
+++ scripts/killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("you died") # Replace with function body.
+	timer.start()
+	
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
